b58e406125667ecbff3807d53aa1ec10a7af28b4
Alessa21 <grossa@tcd.ie>
2015-04-13 23:37:25 +0100

    picture orientation on galaxy s3 fixed

--- InstanityApp/platforms/android/ant-build/classes.dex.d
+++ InstanityApp/platforms/android/ant-build/classes.dex.d
@@ -6,4 +6,7 @@ C:\Users\Gross\Documents\GitHub\AntiUsabilityApp\InstanityApp\platforms\android\
 C:\Users\Gross\Documents\GitHub\AntiUsabilityApp\InstanityApp\platforms\android\ant-build\classes\com\app\Instanity\R$string.class \
 C:\Users\Gross\Documents\GitHub\AntiUsabilityApp\InstanityApp\platforms\android\ant-build\classes\com\app\Instanity\R$xml.class \
 C:\Users\Gross\Documents\GitHub\AntiUsabilityApp\InstanityApp\platforms\android\ant-build\classes\com\app\Instanity\R.class \
+C:\Users\Gross\Documents\GitHub\AntiUsabilityApp\InstanityApp\platforms\android\ant-build\classes\org\apache\cordova\camera\CameraLauncher.class \
+C:\Users\Gross\Documents\GitHub\AntiUsabilityApp\InstanityApp\platforms\android\ant-build\classes\org\apache\cordova\camera\ExifHelper.class \
+C:\Users\Gross\Documents\GitHub\AntiUsabilityApp\InstanityApp\platforms\android\ant-build\classes\org\apache\cordova\camera\FileHelper.class \
 C:\Users\Gross\Documents\GitHub\AntiUsabilityApp\InstanityApp\platforms\android\ant-build\dexedLibs\classes-b4ff0f538f8dc0c8463713a61f31ceab.jar \
